6fe87e7d538800c023e570b005272357915f6161
Owen Rodley <Strigoides@gmail.com>
2013-11-30 15:22:34 +1300

    Print some data from the BMP header

--- src/bmp.c
+++ src/bmp.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include "bmp.h"
 
 void print_bmp(char buf[])
 {
-	buf = buf;
 	puts("It's a BMP!");
+	BMP_header *header = (BMP_header*)buf;
+	printf(" size = %d\n", header->file_size);
+	printf(" offset to pixel array = %d\n", header->pixels_offset);
 }
--- src/bmp.h
+++ src/bmp.h
@@ -2,4 +2,13 @@
 
 #define BMP_SIGNATURE 0x424D
 
+typedef struct BMP_header
+{
+	uint16_t signature;
+	uint32_t file_size;
+	uint16_t reserved1;
+	uint16_t reserved2;
+	uint16_t pixels_offset;
+} __attribute__((packed)) BMP_header;
+
 void print_bmp(char buf[]);
