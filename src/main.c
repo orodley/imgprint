c25e3541c6cfc06393af108902fa4c3f768d00b2
Owen Rodley <Strigoides@gmail.com>
2013-12-01 10:11:13 +1300

    Check for error when calling open()

--- src/main.c
+++ src/main.c
@@ -16,6 +16,11 @@ int main(int argc, char *argv[])
 	}
 
 	int fd = open(argv[1], O_RDONLY);
+	if (fd == -1) {
+		perror("error opening file");
+		return 1;
+	}
+
 	char buf[SIGNATURE_LENGTH];
 	ssize_t bytes = read(fd, buf, SIGNATURE_LENGTH);
 	if (bytes == -1) {
