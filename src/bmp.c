82cb3fd17a26e31f9894521efe90f727c879710b
Owen Rodley <Strigoides@gmail.com>
2013-12-01 12:07:46 +1300

    Support some more bpp values
    Should work for any multiple of 8

--- src/bmp.c
+++ src/bmp.c
@@ -22,10 +22,12 @@ int print_bmp(int fd)
 		return 1;
 	}
 
-	if (header->bits_per_pixel != 24) { // Just support one size for now
-		puts("Error: BMPs with bpp other than 24 are currently unsupported");
+	if (header->bits_per_pixel % 8 != 0) {
+		puts("Error: BMPs with bpp indivisible by 8 are currently unsupported");
 		return 1;
 	}
+
+	size_t bytes_per_pixel = header->bits_per_pixel / 8;
 
 	if (header->compression_method != BI_RGB) {
 		puts("Error: compressed BMPs are currently unsupported");
@@ -44,17 +46,24 @@ int print_bmp(int fd)
 	size_t column = 0;
 	int    row    = header->height - 1;
 
-	size_t row_width = 3 * header->width;
+	size_t row_width = bytes_per_pixel * header->width;
 	if (row_width % 4 != 0)
 		row_width += 4 - (row_width % 4);
 
+	uint32_t max = 0;
+	for (size_t n = 0; n < bytes_per_pixel / 3; n++) {
+		max <<= 8;
+		max |= 0xFF;
+	}
+	max *= 3;
+
 	while (row > 0) {
-		size_t i = row * row_width + (column * 3);
+		size_t i = row * row_width + (column * bytes_per_pixel);
 		uint8_t b = pixels[i];
 		uint8_t g = pixels[i + 1];
 		uint8_t r = pixels[i + 2];
 
-		if (r + g + b < (256 * 3) / 2)
+		if ((uint32_t)(r + g + b) < max / 2)
 			putchar(' ');
 		else
 			putchar('#');
