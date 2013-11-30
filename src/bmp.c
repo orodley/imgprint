4a552124c706f0f39bfded2406c3fe742c68efc4
Owen Rodley <Strigoides@gmail.com>
2013-11-30 20:50:34 +1300

    Print in correct order

--- src/bmp.c
+++ src/bmp.c
@@ -7,6 +7,8 @@ int print_bmp(char buf[])
 	BMP_header *bmp_header = (BMP_header*)buf;
 	DIB_header *dib_header = (DIB_header*)(bmp_header + 1);
 
+	printf("dimensions: %d x %d\n\n", dib_header->width, dib_header->height);
+
 	if (dib_header->num_colors != 0 || dib_header->bits_per_pixel < 8) {
 		puts("Error: BMPs with color tables are currently unsupported");
 		return 1;
@@ -17,9 +19,16 @@ int print_bmp(char buf[])
 		return 1;
 	}
 
-	char *pixels = buf + bmp_header->pixels_offset;
+	char  *pixels = buf + bmp_header->pixels_offset;
 	size_t column = 0;
-	for (size_t i = 0; i < dib_header->pixel_array_size; i += 3) {
+	int    row    = dib_header->height - 1;
+
+	size_t row_width = 3 * dib_header->width;
+	if (row_width % 4 != 0)
+		row_width += 4 - (row_width % 4);
+
+	while (row > 0) {
+		size_t i = row * row_width + (column * 3);
 		uint8_t b = pixels[i];
 		uint8_t g = pixels[i + 1];
 		uint8_t r = pixels[i + 2];
@@ -32,10 +41,7 @@ int print_bmp(char buf[])
 		column++;
 		if (column == dib_header->width) {
 			putchar('\n');
-
-			if (column % 4 != 0)
-				i += 4 - (column % 4);
-
+			row--;
 			column = 0;
 		}
 	}
