04927e52f1db7f53552c61148755234c1cf16a7e
Owen Rodley <Strigoides@gmail.com>
2013-11-30 18:42:05 +1300

    DIB header reading

--- src/bmp.c
+++ src/bmp.c
@@ -4,7 +4,13 @@
 void print_bmp(char buf[])
 {
 	puts("It's a BMP!");
-	BMP_header *header = (BMP_header*)buf;
-	printf(" size = %d\n", header->file_size);
-	printf(" offset to pixel array = %d\n", header->pixels_offset);
+
+	BMP_header *bmp_header = (BMP_header*)buf;
+	printf(" file size = %d\n", bmp_header->file_size);
+	printf(" offset to pixel array = %d\n", bmp_header->pixels_offset);
+
+	DIB_header *dib_header = (DIB_header*)(bmp_header + 1);
+	printf(" dimensions = %d x %d\n", dib_header->width, dib_header->height);
+	printf(" bits per pixel = %d\n", dib_header->bits_per_pixel);
+	printf(" number of colors = %d\n", dib_header->num_colors);
 }
--- src/bmp.h
+++ src/bmp.h
@@ -8,7 +8,30 @@ typedef struct BMP_header
 	uint32_t file_size;
 	uint16_t reserved1;
 	uint16_t reserved2;
-	uint16_t pixels_offset;
+	uint32_t pixels_offset;
 } __attribute__((packed)) BMP_header;
+
+typedef struct DIB_header
+{
+	uint32_t header_size;
+	uint32_t width;
+	uint32_t height;
+	uint16_t planes;
+	uint16_t bits_per_pixel;
+	uint32_t compression_method;
+	uint32_t pixel_array_size;
+	uint32_t horizontal_resolution;
+	uint32_t vertical_resolution;
+	uint32_t num_colors;
+	uint32_t num_important_colors;
+} __attribute__((packed)) DIB_header;
+
+// Compression methods (used in DIB_header.compression_method)
+#define BI_RGB 0       // no compression
+#define BI_RLE8 1      // RLE 8-bit/pixel
+#define BI_RLE4 2      // RLE 4-bit/pixel
+#define BI_BITFIELDS 3 // Bit field or Huffman 1D compression
+#define BI_JPEG 4      // JPEG or RLE-24 compression
+#define BI_PNG 5       // The bitmap contains a PNG image
 
 void print_bmp(char buf[]);
